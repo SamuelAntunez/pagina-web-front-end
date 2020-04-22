cbf2d6f6fd2e4b0cfe0517360bf6773aa5ed3b13
samuel <samueljoseantunez@gmail.com>
2020-04-22 04:03:43 -0500

    terminado

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+     $('#btnAdd').click(function(){
+         alertify.success("El Producto se ha agregado exitosamente a su cartera")
+     })
+})
+
+$(document).ready(function(){
+    $('#buttonCart').click(function(){
+        alertify.success("El Producto se ha agregado exitosamente a su cartera")
+    })
+})
